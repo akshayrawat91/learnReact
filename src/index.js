1298d525a6d3e4590b2b6f9dd3a4ea8fcc882299
Akshay <akxrawat@gmail.com>
2018-07-06 18:55:30 -0500

    react proj

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// creating component (which produces html)
+const App = () => {
+    return <div>Hi</div>;   // this is JSX not html
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// place this component on the page i.e. DOM
+// <App /> is an JSX instance of a class
+ReactDOM.render(<App />, document.querySelector('.container'));
