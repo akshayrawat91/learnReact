2d57cbf7f1940e8cbe10540a120c6a22aeca6644
Akshay <akxrawat@gmail.com>
2018-07-07 18:16:20 -0500

    added states in classses

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyDakOFYlYUDhIU4cHR0kmw5bA_y2Us9qas';
 
 // creating component (which produces html)
-const App = () => {
-    return <div>Hi</div>;   // this is JSX not html
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { videos: [] };
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });   // when both have same name
+            //this.setState({videos: videos});
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos = { this.state.videos } />
+            </div>
+        );   // this is JSX not html
+    }
 }
+    
 
 // place this component on the page i.e. DOM
 // <App /> is an JSX instance of a class
