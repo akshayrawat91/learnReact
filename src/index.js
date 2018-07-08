fe4a50080e347fcc9f012f0eef6c1f519dde136b
Akshay <akxrawat@gmail.com>
2018-07-07 19:48:47 -0500

    added functionalities list not working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDakOFYlYUDhIU4cHR0kmw5bA_y2Us9qas';
 
@@ -10,22 +12,39 @@ const API_KEY = 'AIzaSyDakOFYlYUDhIU4cHR0kmw5bA_y2Us9qas';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = { videos: [] };
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });   // when both have same name
-            //this.setState({videos: videos});
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
+
     render() {
+        //throttle
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+        
         return (
             <div>
-                <SearchBar />
-                <VideoList videos = { this.state.videos } />
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video = { this.state.selectedVideo } />
+                <VideoList 
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                    videos = { this.state.videos } />
             </div>
         );   // this is JSX not html
     }
 }
-    
 
 // place this component on the page i.e. DOM
 // <App /> is an JSX instance of a class
